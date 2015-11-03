b337728627b71dc1e8de5fb4645d4da839af0174
Steven Chamberlin <smc2@us.ibm.com>
2015-11-03 01:48:24 -0800

    Initial commit

---
+++ 98-sqldb-dashdb-cf.js
@@ -0,0 +1,518 @@
+/**
+ * Copyright 2014 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+module.exports = function(RED) {
+
+    var cfEnv = require("cfenv");
+    var appEnv = cfEnv.getAppEnv();
+    var debugLocal = false;
+    var SQLDBservices = [];
+    var dashDBservices = [];
+
+    // load the services bound to this application
+    function extractProperties(v) {
+      return { name: v.name, label: v.label };
+    }
+
+    for (var i in appEnv.services) {
+        // filter for SQLDB services
+        if (i.match(/^(sqldb)/i)) {
+            SQLDBservices = SQLDBservices.concat(appEnv.services[i].map(extractProperties));
+        }
+        // filter for dashDB services
+        else if (i.match(/^(Analytics)/i) || i.match(/^(dashDB)/i)) {
+            dashDBservices = dashDBservices.concat(appEnv.services[i].map(extractProperties));
+        }
+    }
+
+    //
+    // HTTP endpoints that will be accessed from the HTML file
+    //
+    RED.httpAdmin.get('/sqldb/vcap', function(req,res) {
+        res.send(JSON.stringify(SQLDBservices));
+    });
+    
+    RED.httpAdmin.get('/dashDB/vcap', function(req,res) {
+        res.send(JSON.stringify(dashDBservices));
+    });
+
+    function SQLDBOutNode(n) {
+
+        RED.nodes.createNode(this,n);
+
+        var db              = require('ibm_db')();
+        this.table          = n.table;
+        var SQLDBconfig     = appEnv.getService(n.service);
+
+        if (!this.table) {
+	    this.error("SQLDB node configuration error: table not defined");
+	    return;
+	}
+
+	if (!SQLDBconfig) {
+	   this.error("SQLDB node configuration error: service not defined");
+	   return; 
+	}
+
+	var node = this;
+        var db2 = {};
+        if (!debugLocal) {
+           db2.db = SQLDBconfig.credentials.db;               
+           db2.username = SQLDBconfig.credentials.username;
+           db2.hostname = SQLDBconfig.credentials.hostname;
+           db2.password = SQLDBconfig.credentials.password;
+           db2.port = SQLDBconfig.credentials.port;
+        }
+        // This is for debugging locally
+        else {
+           db2 = {
+           db: "TESTDB",
+           hostname: "localhost",
+           port: 50000,
+           username: "db2admin",
+           password: "yourpassword"
+           };
+        }
+
+        var connString = "DRIVER={DB2};DATABASE=" + db2.db + ";UID=" + db2.username + ";PWD=" + db2.password + ";HOSTNAME=" + db2.hostname + ";port=" + db2.port;
+
+        try {
+           console.log("SQLDB output node: Opening db connection...");
+           db.openSync(connString);
+           console.log("SQLDB output node: Connection open");
+	   }
+	catch (e) {
+           node.error(e.message);
+	}
+
+        var columnList = getColumns(node,db,node.table,"SQLDB output node");
+        console.log("SQLDB output node: columnList: " + columnList);
+
+        node.on("close", function() {
+        console.log("SQLDB output node: Closing db connection...");
+        db.closeSync();
+        console.log("SQLDB output node: Connection closed");
+        });
+
+        console.log("columnList: " + columnList);  
+        var questionMarks = genQuestionMarks(columnList);
+        
+        var insertStatement = "insert into "+node.table+"(" + columnList + ") values("+questionMarks+")";
+        console.log("SQLDB output node: Preparing insert statement: " + insertStatement);	
+
+        node.on("input", function(msg) {
+		db.prepare(insertStatement, function (err, stmt) {
+                   if (err) {
+		      node.error("SQLDB output node: " + err);
+		   }
+		   else  {
+		      console.log("SQLDB output node: Prepare successful");
+		      processInput(node,msg,db,stmt,columnList,"SQLDB");
+		   }
+		});
+	});
+     }
+
+    function dashDBOutNode(n) {
+
+        RED.nodes.createNode(this,n);
+
+        var db              = require('ibm_db')();
+        this.table          = n.table;
+        var dashDBconfig    = appEnv.getService(n.service);
+
+        if (!this.table) {
+	    this.error("dashDB node configuration error: table not defined");
+	    return;
+	}
+
+	if (!dashDBconfig) {
+	   this.error("dashDB node configuration error: service not defined");
+	   return; 
+	}
+
+	var node = this;
+        var db2 = {};
+        if (!debugLocal) {
+           db2.db = dashDBconfig.credentials.db;               
+           db2.username = dashDBconfig.credentials.username;
+           db2.hostname = dashDBconfig.credentials.hostname;
+           db2.password = dashDBconfig.credentials.password;
+           db2.port = dashDBconfig.credentials.port;
+        }
+        // This is for debugging locally
+        else {
+           db2 = {
+              db: "TESTDB",
+              hostname: "localhost",
+              port: 50000,
+              username: "db2admin",
+              password: "yourpassword"
+           };
+        }
+
+        var connString = "DRIVER={DB2};DATABASE=" + db2.db + ";UID=" + db2.username + ";PWD=" + db2.password + ";HOSTNAME=" + db2.hostname + ";port=" + db2.port;
+
+        try {
+           console.log("dashDB output node: Opening db connection...");
+           db.openSync(connString);
+           console.log("dashDB output node: Connection open");
+	}
+	catch (e) {
+           node.error(e.message);
+	}
+
+        var columnList = getColumns(node,db,node.table,"dashDB output node");
+        console.log("dashDB output node: columnList: " + columnList);
+
+        node.on("close", function() {
+           console.log("dashDB: Closing db connection...");
+           db.closeSync();
+           console.log("dashDB output node: Connection closed");
+        });
+
+        var questionMarks = genQuestionMarks(columnList);
+        
+        var insertStatement = "insert into "+node.table+"(" + columnList + ") values("+questionMarks+")";
+        console.log("dashDB output node: Preparing insert statement: " + insertStatement);	
+   
+        node.on("input", function(msg) {
+		db.prepare(insertStatement, function (err, stmt) {
+                   if (err) {
+		      node.error("dashDB output node: " + err);
+		   }
+		   else  {
+		      console.log("dashDB output node: Prepare successful");
+		      processInput(node,msg,db,stmt,columnList,"dashDB");
+		   }
+		});
+	});
+     }
+
+RED.nodes.registerType("sqldb out", SQLDBOutNode);
+RED.nodes.registerType("dashDB out", dashDBOutNode);
+
+function getColumns (node,db,table,service) {
+      //Remove the schema, if it exists, hopefully the table name is unique - need to improve this
+      var removeSchema = table.split(".");
+      if (removeSchema.length > 1) { table = removeSchema[1]; }
+      console.log(service+": Fetching column names for table " + table + "...");
+      var sysibmColumns;
+      try {
+         sysibmColumns = db.querySync("select name from sysibm.syscolumns where tbname = '"+table+"' and generated = ''"); 
+      }
+      catch (e) {
+         node.error("Error fetching column list: " + e.message);
+         return -1;
+      }
+
+      if (sysibmColumns.length == 0) {
+         node.error(service+": table "+table+" not found - is it defined?  Case matters."); 
+         return -1;
+      }
+      var columnList = [];
+      for (var i = 0; i < sysibmColumns.length; i++) {
+           columnList.push(sysibmColumns[i].NAME);
+       }
+     return columnList;
+     }
+
+function processInput (node,msg,db,stmt,columnList,service) {
+      console.log(service+": Input event received");
+      console.log(service+": columnList: "+columnList);
+      var valueToInsert;
+      var batchInsert;
+      var valueList;
+      var insertIterations;
+      if (msg.payload instanceof Array) {
+         console.log(service+": msg.payload is an array, need to iterate...");
+         batchInsert = true;
+         insertIterations = msg.payload.length; 
+         }
+      else {
+         console.log(service+": msg.payload not an array"); 
+         batchInsert = false;
+         insertIterations = 1;
+         }
+//      db.beginTransaction(function (err) {
+//         if (err) node.error(service+": "+err);
+         for (var i = 0; i < insertIterations; i++) {
+            valueList = [];
+            for (var j = 0; j < columnList.length; j++) {
+               if (batchInsert == true) valueToInsert = msg.payload[i][columnList[j]];
+               else valueToInsert = msg.payload[columnList[j]];
+               if (valueToInsert != undefined) {
+                  if (valueToInsert == 'TIMESTAMP') {
+                     valueList.push(genDB2Timestamp());
+                     } 
+                  else {
+                     valueList.push(valueToInsert);
+                     }
+                  }
+               else {node.error(service+": Column "+columnList[j]+" is missing from the payload or has an undefined value"); return;}
+            }
+            console.log("Values to execute:");
+            console.log(valueList);
+            stmt.execute(valueList, function (err, result) {
+               if (err) {
+                  node.error(service+": Insert failed: "+err); 
+               } else {
+                  console.log(service+": Insert successful!"); 
+                  result.closeSync();
+               }
+            });
+         }
+//      db.commitTransaction(function(err){
+//      if (err) {
+//              console.log(service+": Error during commit: " + err);
+//           }
+//           else {
+//               console.log(service+": Commit successful");
+//           }
+//         });
+//      });
+   }
+
+function pathToArray(commaSeperatedPaths) {
+   var pathList = commaSeperatedPaths.split(",");
+   var resultArray = new Array(pathList.length);
+   for (var i = 0; i < pathList.length; i++) {
+      var pathString = pathList[i];
+      var fields = pathString.split(".");
+      resultArray[i] = fields;
+   }
+   return resultArray;
+}
+
+
+function extractValues(data, targets) {
+   // dbresult will store all resulting targets
+   data = {"msg":data};
+   var dbresult = [];
+   // loop over all targets
+   for (var target_index=0; target_index<targets.length; target_index++) {
+      var value = data;
+      var target = targets[target_index];
+      // loop over all fields in target
+      for (var field_index=0; field_index<target.length; field_index++) {
+         var field = target[field_index];
+         // Final value will be the result
+         // from the nested data structure
+         value = value[field];
+      }
+      // append value to results
+      dbresult.push(value);
+   }
+   return dbresult;
+}
+
+function genQuestionMarks(colList) {
+	var count = colList.length;
+	var resultString = "";
+	for (var i = 0; i < count; i++) {
+		resultString += "?";
+		if (i < count - 1) {
+			resultString += ", ";
+		}
+	}
+	return resultString;
+}
+
+function genDB2Timestamp() {
+   var d = new Date();
+      return d.getFullYear() + "-" +
+      ("00" + (d.getMonth() + 1)).slice(-2) + "-" +
+      ("00" + d.getDate()).slice(-2) + " " +
+      ("00" + d.getHours()).slice(-2) + ":" +
+      ("00" + d.getMinutes()).slice(-2) + ":" +
+      ("00" + d.getSeconds()).slice(-2);
+}
+
+function SQLDBQueryNode(n) {
+
+        RED.nodes.createNode(this,n);
+        var db              = require('ibm_db')();
+        var query           = n.query;
+        var params          = n.params;
+        var SQLDBconfig     = appEnv.getService(n.service);
+
+        if (!SQLDBconfig) {
+           this.error("SQLDB node configuration error: service not defined");
+           return;
+        }
+
+        var node = this;
+        var db2 = {};
+        if (!debugLocal) {
+           db2.db = SQLDBconfig.credentials.db;
+           db2.username = SQLDBconfig.credentials.username;
+           db2.hostname = SQLDBconfig.credentials.hostname;
+           db2.password = SQLDBconfig.credentials.password;
+           db2.port = SQLDBconfig.credentials.port;
+        }
+        // This is for debugging locally
+        else {
+           db2 = {
+           db: "TESTDB",
+           hostname: "localhost",
+           port: 50000,
+           username: "db2admin",
+           password: "yourpassword"
+           };
+        }
+
+        var connString = "DRIVER={DB2};DATABASE=" + db2.db + ";UID=" + db2.username + ";PWD=" + db2.password + ";HOSTNAME=" + db2.hostname + ";port=" + db2.port;
+
+        try {
+           console.log("SQLDB query node: Opening db connection...");
+           db.openSync(connString);
+           console.log("SQLDB query node: connection open");
+           }
+        catch (e) {
+           node.error(e.message);
+        }
+
+        node.on("close", function() {
+           console.log("SQLDB query node: Closing db connection...");
+           db.closeSync();
+           console.log("SQLDB query node: Connection closed");
+        });
+
+
+        this.on('input', function(msg) {
+           if (query == "" || query == null) {
+              query = msg.payload; 
+              if (msg.payload == "" || msg.payload == null) {
+                 node.error("SQLDB out node: msg.payload is empty!");
+                 return;
+                 } 
+           }
+           var parameterValues=[];
+           if (params != "" && params != null) {
+              var path = pathToArray(params.toString());
+              console.log("Input node: pathToArray: " + path);
+              parameterValues = extractValues(msg, path);
+              console.log("Input node: parameterValues: " + parameterValues);
+           }
+           db.query(query,parameterValues,function (err, rows, moreResultSets) {
+              if (err) {
+                 console.log("Error fetching rows: " + err);
+              } else {
+                 console.log("Fetching rows: " + rows);
+                 console.log("value 1: " + JSON.stringify(rows[0]));
+                 if (rows.length == 1) {queryresult = rows[0];}
+                 else {
+                    queryresult = []; 
+                    for (var i = 0; i < rows.length; i++) {
+                       queryresult.push(rows[i]);
+                    }
+                 }
+            msg.payload = queryresult;
+            node.send(msg);
+           }
+        });
+      });
+    }
+    RED.nodes.registerType("sqldb in",SQLDBQueryNode);
+
+
+function dashDBQueryNode(n) {
+
+        RED.nodes.createNode(this,n);
+        var db              = require('ibm_db')();
+        var query           = n.query;
+        var params          = n.params;
+        var dashDBconfig    = appEnv.getService(n.service);
+
+        if (!dashDBconfig) {
+           this.error("dashDB query node configuration error: service not defined");
+           return;
+        }
+
+        var node = this;
+        var db2 = {};
+        if (!debugLocal) {
+           db2.db = dashDBconfig.credentials.db;
+           db2.username = dashDBconfig.credentials.username;
+           db2.hostname = dashDBconfig.credentials.hostname;
+           db2.password = dashDBconfig.credentials.password;
+           db2.port = dashDBconfig.credentials.port;
+        }
+        // This is for debugging locally
+        else {
+           db2 = {
+           db: "TESTDB",
+           hostname: "localhost",
+           port: 50000,
+           username: "db2admin",
+           password: "yourpassword"
+           };
+        }
+
+        var connString = "DRIVER={DB2};DATABASE=" + db2.db + ";UID=" + db2.username + ";PWD=" + db2.password + ";HOSTNAME=" + db2.hostname + ";port=" + db2.port;
+
+        try {
+           console.log("dashDB query node: Opening db connection...");
+           db.openSync(connString);
+           console.log("dashDB query node: Connection open");
+           }
+        catch (e) {
+           node.error(e.message);
+        }
+
+        node.on("close", function() {
+           console.log("dashDB query node: Closing db connection...");
+           db.closeSync();
+           console.log("dashDB query node: Connection closed");
+        });
+
+
+        this.on('input', function(msg) {
+           if (query == "" || query == null) {
+              if (msg.payload == "" || msg.payload == null) {
+                 node.error("dashDB out node: msg.payload is empty!");
+                 return;
+                 } 
+              query = msg.payload; 
+           }
+           var parameterValues=[];
+           if (params != "" && params != null) {
+              var path = pathToArray(params.toString());
+              console.log("Input node: pathToArray: " + path);
+              parameterValues = extractValues(msg, path);
+              console.log("Input node: parameterValues: " + parameterValues);
+           }
+           db.query(query,parameterValues,function (err, rows, moreResultSets) {
+              if (err) {
+                 console.log("Error fetching rows: " + err);
+              } else {
+                 console.log("Fetching rows: " + rows);
+                 console.log("value 1: " + JSON.stringify(rows[0]));
+                 if (rows.length == 1) {queryresult = rows[0];}
+                 else {
+                    queryresult = []; 
+                    for (var i = 0; i < rows.length; i++) {
+                       queryresult.push(rows[i]);
+                    }
+                 }
+            msg.payload = queryresult;
+            node.send(msg);
+           }
+        });
+      });
+    }
+  RED.nodes.registerType("dashDB in",dashDBQueryNode);
+
+};
---
+++ README.md
@@ -0,0 +1,85 @@
+node-red-node-cf-sqldb
+=========================
+[Node-RED](http://nodered.org) nodes to work with a database 
+in a SQLDB or dashDB service that is integrated with
+[IBM Bluemix](http://bluemix.net).
+
+Install
+-------
+Install from [npm](http://npmjs.org)
+```
+npm install node-red-nodes-cf-sqldb-dashdb
+```
+
+Usage
+-----
+This package contains 4 nodes -- two pairs of nodes for each of SQLDB and dashDB services.  The node pairs work almost identically, so the documentation contained here
+applies to both.  For each service, a query and an output node is provided.  The query nodes let you issue a query against
+your SQLDB or dashDB service and pass along the result set in the msg object.  The output nodes store elements from the msg object
+into your SQLDB or dashDB service database.
+
+
+Query node usage:
+-----------------
+
+You will need to fill in the following fields:
+
+-- Service should point to your SQLDB or dashDB service.
+
+-- Query is the SQL select query you wish to execute on your dashDB service database.  If the
+field is empty, it will get the query from the msg.payload.
+
+These fields are optional:
+
+-- Parameter Markers is a comma delimited set of json paths.  These will replace any question
+marks that you place in your SQL query, in the order that they appear.
+
+-- Name: Optionally give your node a name
+
+The results of your query will be returned as a JSON array in msg.payload.
+
+
+Output node usage:
+------------------
+
+The output node will get the values to insert from msg.payload.  
+
+So for example, you might create a function node that flows into your sqldb output node
+with code like this:
+
+```
+msg.payload = 
+{
+  TS : 'TIMESTAMP',
+  SCREENNAME : msg.tweet.user.screen_name,
+  TWEET : msg.payload,
+  SENTIMENT : msg.sentiment.score,
+  LOCATION : msg.location
+}
+return msg;
+```
+
+This would assume that you have a table already created with columns (TS, SCREENNAME, TWEET, SENTIMENT, LOCATION).
+'TIMESTAMP' string is a special keyword that the node will replace with a real DB2 timestamp before insertion.
+If you are missing a required column, an error will be displayed in the debug view.
+
+For output node configuration, you will need to fill in the following fields:
+
+-- Service should point to your SQLDB or dashDB service.
+
+-- Table should point to the table you wish to insert the values into.
+This table needs to exist already in the database and needs to contain the
+same number of columns that you are inserting through this node. The data
+types of the columns have to match. The data coming into the node through 
+the msg.payload needs to be in the format accepted by DB2 and within the 
+appropriate ranges/parameters for that datatype.
+
+The following fields are optional:
+
+-- Name: Optionally give your node a name, otherwise the default will be the table name.
+
+
+Authors
+-------
+* Steven Chamberlin - [smc2@us.ibm.com](mailto:smc2@us.ibm.com)
+* Nicholas Vargas 
