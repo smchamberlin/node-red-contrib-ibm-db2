c8422339ad809de64d4fbc61fd7d0e8bc7869895
Ross Cruickshank <ross.cruickshank@uk.ibm.com>
2020-10-22 12:24:52 +0100

    assume SSL unless port=50000
    new DB2 on Cloud implementation (replacing existing dashdb-style by end of October 2020) assigns ephemeral ports in 30000+ range, and requires SSL only -- switch from non-SSL except for 50001, to non-ssl only for 50000

--- 98-ibm-db2.js
+++ 98-ibm-db2.js
@@ -104,7 +104,7 @@ module.exports = function (RED) {
         Db2config.username +
         ";PWD=" +
         Db2config.password;
-      if (Db2config.port == 50001) {
+      if (Db2config.port != 50000) {
         connString = connString + ";Security=SSL";
       }
     }
@@ -369,7 +369,7 @@ module.exports = function (RED) {
         Db2config.username +
         ";PWD=" +
         Db2config.password;
-      if (Db2config.port == 50001) {
+      if (Db2config.port != 50000) {
         connString = connString + ";Security=SSL";
       }
     }
